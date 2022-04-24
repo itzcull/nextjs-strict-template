25ddeff12c0d30c68de7f41eff70e7f1028918fc
Will Cullen <itzcull@protonmail.com>
2022-04-24 12:26:12 +1000

    Reorganised components for vanilla-extract design patterns

--- layouts/main.tsx
+++ layouts/main.tsx
@@ -1,16 +1,13 @@
-import Footer from "components/Footer"
-import Header from "components/Header"
-import { NextPage } from "next"
+import Footer from 'components/Footer'
+import Header from 'components/Header'
+import { NextPage } from 'next'
 
-const MainLayout: NextPage = ({ children }) => {
-  // Heading & Footer
-  return (
-    <>
-      <Header />
-      {children}
-      <Footer />
-    </>
-  )
-}
+const MainLayout: NextPage = ({ children }) => (
+  <>
+    <Header />
+    {children}
+    <Footer />
+  </>
+)
 
 export default MainLayout
